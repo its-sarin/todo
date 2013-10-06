8814e2c44f30842e2a4d9079f6dec969d0defad9
Anthony Recchia <anthonyrecchia@Anthonys-MacBook-Air.local>
2013-10-06 13:15:04 -0400

    Add functionality to track tasks by id rather than $index

--- js/controllers/taskctrl.js
+++ js/controllers/taskctrl.js
@@ -1,12 +1,18 @@
 app.controller('TaskCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
 	var ref = new Firebase('https://tonyrecchia.firebaseio.com/todotest');
 	$scope.tasks = [];
+	newTodo = {};
 	angularFire(ref, $scope, "tasks");
 
 	$scope.addTask = function(e)	{
 		if (e.keyCode != 13) return;
-
-		$scope.tasks.push({task: $scope.todo});
+		console.log(e.srcElement.value);
+		
+		newTodo.value = $scope.todo;
+		newTodo.id = new Date().getUTCMilliseconds();
+		console.log(newTodo.id);
+		
+		$scope.tasks.push({task: newTodo.value, id: newTodo.id});
 		$scope.todo = "";
 	}
 
