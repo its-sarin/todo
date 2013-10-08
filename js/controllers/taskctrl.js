cea4f829a6b34005bae4e720d65bd0a600971338
Tony Recchia <tonyrecchia@me.com>
2013-10-08 11:28:15 -0400

    Fix new list submitting w/ empty string, add color to brand

--- js/controllers/taskctrl.js
+++ js/controllers/taskctrl.js
@@ -1,17 +1,21 @@
 app.controller('TaskCtrl', ['$scope', '$rootScope', '$location', 'angularFire', '$window', 'authService', function ($scope, $rootScope, $location, angularFire, $window, authService) {	
 	console.log($location.host());
 
+	// Store location path to use for creating list in database
 	var listId = $location.path();
 	console.log(listId);
 
+	// Create Firebase reference to 'incomplete' items
 	var tasksRef = new Firebase('https://listify.firebaseio.com/lists' +listId + '/incomplete');
 	$scope.tasks = [];
 	angularFire(tasksRef, $scope, "tasks");
 
+	// Create Firebase reference to 'completed' items
 	var completedTasksRef = new Firebase('https://listify.firebaseio.com/lists' + listId + '/completed');
 	$scope.completedTasks = [];		
 	angularFire(completedTasksRef, $scope, "completedTasks");
 
+	// Create Firebase reference to 'users' if user authenticates
 	$rootScope.$on("login", function(event, user) {		
         var userRef = new Firebase('https://listify.firebaseio.com/lists' +listId + '/users/')
         console.log(user.uid);
@@ -21,7 +25,9 @@ app.controller('TaskCtrl', ['$scope', '$rootScope', '$location', 'angularFire', 
     });
 
 	var newTodo = {};
+	$scope.newListName = '';
 
+	// Creates a new list
 	$scope.createNewList = function()	{
 		if (/^ *$/.test($scope.newListName)) return;
 
