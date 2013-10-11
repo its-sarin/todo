a6e8f90300ac86c99e0f4d1ecac6f5883ccb74ea
Tony Recchia <tonyrecchia@me.com>
2013-10-11 19:01:49 -0400

    fix: move 'newList' function into NavCtrl

--- js/controllers/taskctrl.js
+++ js/controllers/taskctrl.js
@@ -26,24 +26,6 @@ app.controller('TaskCtrl', ['$scope', '$rootScope', '$location', 'angularFire', 
 		angularFire(userRef, $scope, "users");        
     });
 
-	// Creates a new list
-	$scope.createNewList = function()	{
-		if (/^ *$/.test($scope.newListName)) return;
-
-		// Filters out symbols and whitespace and builds list name
-		var input = $scope.newListName.replace(/-/g,' ').replace(/[^\w\s]/gi, ''),
-			ms = new Date().getUTCMilliseconds(),
-			dt = new Date().getUTCDay(),
-			rn = Math.floor(Math.random()*10);
-			list = input + '-' + ms + dt + rn;
-
-		var authRef = new Firebase('https://listify.firebaseio.com/lists/' + list + '/auth');
-		authRef.set({authorized: 'true'});
-		// Open new list in new window
-		$window.open('/#' + list);
-		$scope.newListName = '';
-	}
-
 	// Adds a task to $scope.tasks and attaches a timestamped id
 	$scope.addTask = function(event)	{
 		var key = event.char || event.keyCode,	// Firefox support
