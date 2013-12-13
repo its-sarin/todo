67ed0836bdc4171a3c4f35224a457c358ed5e7d0
Anthony Recchia <anthonyrecchia@Anthonys-MacBook-Air.local>
2013-12-13 18:47:09 -0500

    Fixes and improves UI sortable

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('todoApp', ['ui', 'firebase']);
+var app = angular.module('todoApp', ['ui.sortable', 'firebase']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 	
